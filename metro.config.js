d86ef85715c2c5350f6ec303eabcb8f7dd6b0b6a
onspace.ai <onspace.ai@gmail.com>
2026-09-25 22:20:54 +0330

    Fix metro.config.js blockList

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-const templateDir = path.resolve(__dirname, 'template');
-config.resolver.blockList = new RegExp(
-  `^${templateDir.replace(/[/\\]/g, '[/\\\\]')}.*`
-);
 
 module.exports = config;
