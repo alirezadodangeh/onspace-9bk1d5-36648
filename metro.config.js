e2f23a7a35e754da0e37723663f1ee4368431dc1
onspace.ai <onspace.ai@gmail.com>
2026-09-25 22:20:13 +0330

    Fix metro.config.js blockList

--- metro.config.js
+++ metro.config.js
@@ -3,11 +3,9 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Exclude the template/ folder from Metro's dependency tree
-// to prevent version mismatch errors with platform-internal packages
-const { exclusionList } = require('metro-config');
-config.resolver.blockList = exclusionList([
-  new RegExp(`${path.resolve(__dirname, 'template').replace(/\\/g, '\\\\')}.*`),
-]);
+const templateDir = path.resolve(__dirname, 'template');
+config.resolver.blockList = new RegExp(
+  `^${templateDir.replace(/[/\\]/g, '[/\\\\]')}.*`
+);
 
 module.exports = config;
