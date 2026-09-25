349efffe2e3d1d2fc7dc74cd928780735b9d09d2
onspace.ai <onspace.ai@gmail.com>
2026-09-25 17:50:43 +0330

    Fix Metro parse error in normalizeDigits

--- services/extractionService.ts
+++ services/extractionService.ts
@@ -13,8 +13,8 @@ export interface ExtractedData {
 // Normalize Persian/Arabic digits to English
 function normalizeDigits(text: string): string {
   return text
-    .replace(/[۰-۹]/g, (d) => String('۰۱۲۳۴۵۶۷۸۹'.indexOf(d)))
-    .replace(/[٠-٩]/g, (d) => String('٠١٢٣٤٥٦٧٨٩'.indexOf(d)));
+    .replace(/[\u06F0-\u06F9]/g, (d) => String(d.charCodeAt(0) - 0x06F0))
+    .replace(/[\u0660-\u0669]/g, (d) => String(d.charCodeAt(0) - 0x0660));
 }
 
 export function extractData(rawText: string): ExtractedData {
